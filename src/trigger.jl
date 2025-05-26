862aa193ec92cf06c971500df510757587c73fa8
jakezw <jake.zwart@spectrum-tec.com>
2025-05-26 15:16:57 -0400

    update trigger.jl

--- src/trigger.jl
+++ src/trigger.jl
@@ -1,29 +1,34 @@
-using Gpiod: Pi, setup, OUTPUT, INPUT
+using Gpiod: Pi, setup, OUTPUT, INPUTS
 
-p = Pi()
+#= 
+Used the dev version of this package as it has the libgpiod.so file contained within it.
+See issue for Gpio.jl on github for specifics
+=#
+
 
 """
-    function trigger(pin::Int; duration::Real=5)
+    function trigger(pin::Int; duration::Real=0.050)
 
 The pin numbers to use are the GPIO pin numbers.  Thus 23 is 
 GPIO23 which is the Pi pin number 16.  The pin number reference
 is available from the web or the Pi command pinout.
 
-Note that on a fresh build of Gpiod the function setup in Gpiod.jl needs 
-the following edit just befor return to work.
-# pi.request[Cuint(offset)] = request  # original line
-pi.request[Cuint(offset)] = isempty(pi.request) ? request : pi.request[Cuint(offset)]   # edited line
+Alternative Packages to try:
+    https://github.com/notinaboat/PiGPIOMEM.jl/blob/master/src/PiGPIOMEM.jl
+    PiGPIO.jl
+    etc.
 """
-function trigger(pin::Int; duration::Real = 0.020)
+function trigger(pin::Int; duration::Real = 0.050)
+    p = Pi()
+    setup(p, pin, OUTPUT)
     try
-        setup(p, pin, OUTPUT)
         write(p, pin, true)
         sleep(duration)
         write(p, pin, false)
     catch
-        error("Could not trigger MCC172")
-    finally
-        sleep(0.001)
-        setup(p, pin, INPUT)
+        error("")
+    # finally
+	# sleep(0.1)
+        # setup(p, pin, INPUT)
     end
 end
